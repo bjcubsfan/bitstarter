cb308157e8dedc430a1a165f0acf4c15a2ccf600
B. J. Potter <bjpotter@gmail.com>
2013-07-06 17:04:07 -0500

    ref variable correctly

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var index_file = fs.readFileSync('index.html');
 var index_content = index_file.toString();
 console.log(index_content);
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(index_content);
 });
 
 var port = process.env.PORT || 5000;
